e025c3e9a3122aa35eb94348d956231c984b20d8
DengFeng.Su <sword_lord@foxmail.com>
2021-09-13 16:22:07 +0800

    增加 gitignore

--- Fucking Algorithm/Normal.swift
+++ Fucking Algorithm/Normal.swift
@@ -15,3 +15,39 @@ func sumOfTwoNum(_ a:Int, _ b:Int) -> Int {
     
     return sumOfTwoNum(a^b, (a&b)<<1)
 }
+
+// leecode 50 计算a的n次幂
+func pow(_ a:Double, _ n:Int) -> Double {
+    
+//    return n > 0 ? pow1(a, n):1.0/pow1(a, -n)
+    
+    return n > 0 ? pow2(a, n):1.0/pow2(a, -n)
+}
+
+// 递归
+func pow1(_ a:Double, _ n:Int) -> Double {
+    if 0 == n {
+        return 1
+    }
+    let b = pow1(a, n/2)
+    return (0 == n%2) ? b*b:b*b*a;
+}
+// 迭代
+func pow2(_ a:Double, _ n:Int) -> Double {
+    
+    var tmpN = n
+    var tmpA = a
+    var res = 1.0
+    
+    while tmpN > 0 {
+        
+        if 1 == tmpN%2 {
+            res *= tmpA
+        }
+        tmpA *= tmpA
+        
+        tmpN /= 2
+    }
+    
+    return res
+}
