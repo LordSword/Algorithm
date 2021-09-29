3a5624baca8f1fc8751abe48c78a5b3fcb0c751e
DengFeng.Su <sword_lord@foxmail.com>
2021-09-29 16:16:06 +0800

    leecode 517

--- Fucking Algorithm/Array.swift
+++ Fucking Algorithm/Array.swift
@@ -9,6 +9,36 @@ import Cocoa
 
 extension Array {
     
+    // leecode 517. 超级洗衣机
+    // 假设有 n 台超级洗衣机放在同一排上。开始的时候，每台洗衣机内可能有一定量的衣服，也可能是空的。
+    // 在每一步操作中，你可以选择任意 m (1 <= m <= n) 台洗衣机，与此同时将每台洗衣机的一件衣服送到相邻的一台洗衣机。
+    // 给定一个整数数组 machines 代表从左至右每台洗衣机中的衣物数量，请给出能让所有洗衣机中剩下的衣物的数量相等的 最少的操作步数 。如果不能使每台洗衣机中衣物的数量相等，则返回 -1 。
+    func findMinMoves(_ machines: [Int]) -> Int {
+        
+        var sum = 0
+        for i in machines {
+            sum += i
+        }
+        
+        guard 0 == sum%machines.count else {
+            return -1
+        }
+        
+        let val = sum/machines.count
+        var count = 0
+        var res = 0
+        
+        for i in machines {
+            // 加上平均值, 计算当前剩余值
+            let num = (i - val)
+            count += num
+            
+            res = Swift.max(res, num, abs(count))
+        }
+
+        return res
+    }
+    
     // leecode-2196
     // 含有重复元素集合的组合
     // 给定一个可能有重复数字的整数数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
